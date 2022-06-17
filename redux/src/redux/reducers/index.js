17d0bc94eb2bf234d5c2f10cd785ffbe06a502cb
Orkhan Ganbarov <tu201906147@code.edu.az>
2022-06-18 01:56:57 +0400

    added and refreshed file

---
+++ redux/src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import changeCategoryReducer from "./changeCategoryReducer"
+
+const rootreducer = combineReducers({
+    changeCategoryReducer
+})
+
+export default rootreducer;
